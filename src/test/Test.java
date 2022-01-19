a6ff39e6a5f047e0bee05f476811a530c777d347
DVPesh <dvpesh@mail.ru>
2022-01-19 03:41:15 +0300

    add static method is added

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,7 +1,15 @@
 package test;
 
 public class Test {
+
     public static void main(String[] args) {
         System.out.println("Test");
+        int a = 15, b = 25;
+        System.out.printf("%d + %d = %d%n", a, b, add(a, b));
     }
+
+    private static int add(int a, int b) {
+        return a + b;
+    }
+
 }
