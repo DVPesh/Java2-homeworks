48b9efd5589029a0dd2954ff77d7ada43807631b
DVPesh <dvpesh@mail.ru>
2022-01-19 03:45:05 +0300

    sub static method is added

--- src/test/Test.java
+++ src/test/Test.java
@@ -6,10 +6,15 @@ public class Test {
         System.out.println("Test");
         int a = 15, b = 25;
         System.out.printf("%d + %d = %d%n", a, b, add(a, b));
+        System.out.printf("%d - %d = %d%n", a, b, sub(a, b));
     }
 
     private static int add(int a, int b) {
         return a + b;
+    }
+
+    private static int sub(int a, int b) {
+        return a - b;
     }
 
 }
